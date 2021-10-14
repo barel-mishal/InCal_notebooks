125696d0faf5bfd58d2681e1596b9683a44a2a3a
dreamwork-Barel-and-Yuval <dreamwork@dreamwork.network>
2021-10-14 11:44:15 +0300

    first commit - add files to the repo

---
+++ requirement.ps1
@@ -0,0 +1,15 @@
+
+# dask==2021.9.1
+# numpy==1.21.2
+# pandas==1.3.3
+# plotly==5.3.1
+# rpy2==3.4.5
+# statsmodels==0.13.0
+.\venv_incal_notebooks\Scripts\python.exe -m pip install --upgrade pip
+pip install nbformat
+pip install dask
+pip install pandas
+pip install numpy
+pip install plotly
+pip install statsmodels
+pip install rpy2
